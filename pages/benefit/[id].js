3c9c4e8483b6412c4ee531141a7ba2f8380d3c38
Thomas Dacey <DaceyTom2@gmail.com>
2021-04-07 11:50:28 -0400

    added template so page doesn't break

---
+++ pages/benefit/[id].js
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import styles from "../../styles/Home.module.css";
+import { useRouter } from "next/router";
+
+export default function Home() {
+  const router = useRouter();
+  const { id } = router.query;
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Welcome to Page {id}!</h1>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  );
+}
