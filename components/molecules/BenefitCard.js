37d11fdbaa413dc11c2b79ae910672838241f40c
var-kyle <kyle.meelker@gmail.com>
2021-04-08 13:19:29 -0400

    added unit tests for benefit card

--- components/molecules/BenefitCard.js
+++ components/molecules/BenefitCard.js
@@ -1,22 +1,20 @@
-import Link from "next/link";
 import PropTypes from "prop-types";
 
 export function BenefitCard(props) {
   return (
-    <div class="bg-white shadow overflow-hidden sm:rounded-lg">
-      <div class="px-4 py-5 sm:px-6">
-        <h3 class="text-lg leading-6 font-medium text-gray-900">
+    <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+      <div className="px-4 py-5 sm:px-6">
+        <h3 className="text-lg leading-6 font-medium text-gray-900">
           {props.title}
         </h3>
-        <p class="mt-1 max-w-2xl text-sm text-gray-500">{props.description}</p>
+        <p className="mt-1 max-w-2xl text-sm text-gray-500">
+          {props.description}
+        </p>
       </div>
-      <div class="border-t border-gray-200">
-        <Link href={`/benefit/${props.id}`}>
-          <a>More info</a>
-        </Link>
-        <Link href={props.applyLink}>
-          <a>Apply now!</a>
-        </Link>
+      <div className="border-t border-gray-200">
+        {/* TODO: these links will be replaced by a component when they are created */}
+        <a href={`/benefit/${props.id}`}>More info</a>
+        <a href={props.applyLink}>Apply now!</a>
       </div>
     </div>
   );
