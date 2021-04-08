509fe7629e1163e7246d13b065cebf63cac65406
Thomas Dacey <DaceyTom2@gmail.com>
2021-04-08 09:07:50 -0400

    implemented banner in details

--- pages/benefit/[id].js
+++ pages/benefit/[id].js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "../../styles/Home.module.css";
 import { useRouter } from "next/router";
+import { Banner } from "../../components/atoms/Banner";
 
 export default function Home() {
   const router = useRouter();
@@ -8,12 +9,12 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>{id}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to Page {id}!</h1>
+        <Banner siteTitle={id}></Banner>
       </main>
 
       <footer className={styles.footer}>
