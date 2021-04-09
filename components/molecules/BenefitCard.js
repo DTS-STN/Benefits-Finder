6e6221236a2fbe3e4d9a32060777ce9ac92c2c3b
var-kyle <kyle.meelker@gmail.com>
2021-04-09 12:18:06 -0400

    updated some styling and the home page

--- components/molecules/BenefitCard.js
+++ components/molecules/BenefitCard.js
@@ -2,14 +2,14 @@ import PropTypes from "prop-types";
 
 export function BenefitCard(props) {
   return (
-    <div className="flex flex-col flex-grow w-full md:w-64 lg:w-1/4 rounded-lg shadow pt-5 pb-5 pl-6 pr-6 m-1">
+    <div className="flex flex-col flex-grow w-full md:w-64 lg:w-1/4 rounded-lg shadow pt-5 pb-5 pl-6 pr-6 m-1 hover:bg-gray-300">
       <div className="flex justify-between items-center w-full">
         <div className="flex flex-col justify-start">
           <h3 className="text-l">{props.title}</h3>
           <small>{props.type}</small>
         </div>
       </div>
-      <p className="m-auto truncate-4-lines w-full">{props.description}</p>
+      <p className="truncate-4-lines ">{props.description}</p>
 
       <div className="bg-bg-gray-dk">
         {/* TODO: these links will be replaced by a component when they are created */}
