e8691815bad31f56abd8136bfd6d04bccfca5b00
Thomas Dacey <DaceyTom2@gmail.com>
2021-04-09 13:20:58 -0400

    next-rosetta implemented

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import { I18nProvider } from "next-rosetta";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <I18nProvider table={pageProps.table}>
+      <Component {...pageProps} />
+    </I18nProvider>
+  );
 }
 
 export default MyApp;
