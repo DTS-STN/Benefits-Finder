7ea9d51399868b3cb2fe3469af6dc427ff88b61e
var-kyle <kyle.meelker@gmail.com>
2021-04-12 13:30:33 -0400

    initial changes to styling

--- components/molecules/BenefitCard.js
+++ components/molecules/BenefitCard.js
@@ -7,23 +7,19 @@ export function BenefitCard(props) {
     window.location.assign(`/benefit/${props.id}`);
   }
   return (
-    <div className="flex flex-col flex-grow w-full md:w-64 lg:w-1/4 rounded-lg shadow pt-5 pb-5 pl-6 pr-6 m-1 hover:bg-gray-300">
-      <div className="flex justify-between items-center w-full">
+    <div className="flex flex-col md:w-64 lg:w-1/4 rounded-lg border border- pt-5 pb-5 pl-6 pr-6 m-1 hover:bg-gray-300">
+      <small className="text-gray-500">{props.type}</small>
+      <div className="flex w-full">
         <div className="flex flex-col justify-start">
           <h3 className="text-l">{props.title}</h3>
-          <small>{props.type}</small>
         </div>
       </div>
       <p className="truncate-4-lines ">{props.description}</p>
 
       <div className="bg-bg-gray-dk">
         {/* TODO: these links will be replaced by a component when they are created */}
-        <ActionButton text="More info" onClick={moreInfoHandler} />
-        <Link href={`/benefit/${props.id}`}>
-          <a>More info</a>
-        </Link>
-        {/* <a href={`/benefit/${props.id}`}>More info</a> */}
-        {/* <a href={props.applyLink}>Apply now!</a> */}
+        <a href={`/benefit/${props.id}`}>More info</a>
+        <a href={props.applyLink}>Apply now!</a>
       </div>
     </div>
   );
