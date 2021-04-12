214917a8fbeb11b58ab09d032bafbd4a23bcbe0f
var-kyle <kyle.meelker@gmail.com>
2021-04-12 11:34:56 -0400

    testing different link types on benefit card

--- components/molecules/BenefitCard.js
+++ components/molecules/BenefitCard.js
@@ -1,6 +1,11 @@
+import { ActionButton } from "../atoms/ActionButton";
+import Link from "next/link";
 import PropTypes from "prop-types";
 
 export function BenefitCard(props) {
+  function moreInfoHandler() {
+    window.location.assign(`/benefit/${props.id}`);
+  }
   return (
     <div className="flex flex-col flex-grow w-full md:w-64 lg:w-1/4 rounded-lg shadow pt-5 pb-5 pl-6 pr-6 m-1 hover:bg-gray-300">
       <div className="flex justify-between items-center w-full">
@@ -13,8 +18,12 @@ export function BenefitCard(props) {
 
       <div className="bg-bg-gray-dk">
         {/* TODO: these links will be replaced by a component when they are created */}
-        <a href={`/benefit/${props.id}`}>More info</a>
-        <a href={props.applyLink}>Apply now!</a>
+        <ActionButton text="More info" onClick={moreInfoHandler} />
+        <Link href={`/benefit/${props.id}`}>
+          <a>More info</a>
+        </Link>
+        {/* <a href={`/benefit/${props.id}`}>More info</a> */}
+        {/* <a href={props.applyLink}>Apply now!</a> */}
       </div>
     </div>
   );
