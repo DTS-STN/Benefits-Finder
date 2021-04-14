e588457bfdaeab38a92dda38e7c82303a0c4d06d
Kyle Meelker <34345435+var-kyle@users.noreply.github.com>
2021-04-14 15:30:05 -0400

    add footer component (#31)

---
+++ components/molecules/Footer.stories.js
@@ -0,0 +1,12 @@
+import { Footer } from "./Footer";
+
+export default {
+  title: "Components/Molecules/Footer",
+  component: Footer,
+};
+
+const Template = (args) => <Footer {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {};
