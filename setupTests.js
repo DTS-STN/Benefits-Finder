6d8ca4ac3cf6a202bb8706037988f8c635642f18
Thomas Dacey <DaceyTom2@gmail.com>
2021-04-14 14:20:04 -0400

    moch i18n for jest

--- setupTests.js
+++ setupTests.js
@@ -1,4 +1,6 @@
 import "@testing-library/jest-dom/extend-expect";
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
 
 // need to mock next/link because it throws an error when using the real one
 // and I don't really know why
@@ -7,3 +9,16 @@ jest.mock("next/link", () => {
     return children;
   };
 });
+
+// Mock react-i18next
+i18n.use(initReactI18next).init({
+  lng: 'en',
+  fallbackLng: 'en',
+  ns: ['common'],
+  defaultNS: 'common',
+  resources: {
+    en: {
+      common: {}
+    }
+  }
+}) 
