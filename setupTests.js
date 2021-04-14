2d1a2d4abeece6234b1af599c5d372f3c921e03e
Thomas Dacey <DaceyTom2@gmail.com>
2021-04-14 16:06:54 -0400

    moch translations

--- setupTests.js
+++ setupTests.js
@@ -1,6 +1,8 @@
 import "@testing-library/jest-dom/extend-expect";
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
 // need to mock next/link because it throws an error when using the real one
 // and I don't really know why
@@ -10,6 +12,15 @@ jest.mock("next/link", () => {
   };
 });
 
+// Mock t()
+jest.mock('next-i18next', () => ({
+  useTranslation: () => {
+    return {
+      t: (str) => str,
+    };
+  },
+}));
+
 // Mock react-i18next
 i18n.use(initReactI18next).init({
   lng: 'en',
@@ -18,7 +29,7 @@ i18n.use(initReactI18next).init({
   defaultNS: 'common',
   resources: {
     en: {
-      common: {}
+      common: { }
     }
   }
-}) 
+})
