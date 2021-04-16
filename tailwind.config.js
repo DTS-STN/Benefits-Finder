03056ed72e6075c1a5b149ad446fde4bd0d0d137
var-kyle <kyle.meelker@gmail.com>
2021-04-16 10:25:52 -0400

    update index page to use layout component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
-const colors = require("tailwindcss/colors");
-
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      display: ["Lato", "sans-serif"],
-      body: ["Noto", "sans-serif"],
+      display: ["Lato"],
+      body: ["Noto sans"],
     },
     fontSize: {
       xxs: ["13px", "16px"],
@@ -34,54 +32,36 @@ module.exports = {
         "footer-parliament-image": "url(../public/landscape.png)",
       }),
       colors: {
-        // extended base Tailwind colors
-        gray: colors.trueGray,
-        coolGray: colors.coolGray,
-        blueGray: colors.blueGray,
-        trueGray: colors.trueGray,
-        warmGray: colors.warmGray,
-        lightBlue: colors.lightBlue,
-        cyan: colors.cyan,
-        lime: colors.lime,
-        emerald: colors.emerald,
-        // Background colors
-        "dk-blue": "#000A3D",
-        "blue-1": "#131F4C",
-        "blue-2": "#4F5B8B",
-        "lt-blue": "#C7CFEF",
         "custom-blue": {
           blue: "#2572B4",
           light: "#1492B4",
           dark: "#26374a",
           link: "#0535d2",
         },
-
-        "lt-gray": "#F8F8F8",
-        "md-lt-gray": "#EFEFEF",
-        "md-gray": "#DEDEDE",
-
-        // Text Colors
-        "font-dk": "#252525",
-        "font-gray": "#636367",
-        link: {
-          DEFAULT: "#284162",
-          selected: "#0A75C6",
-          visited: "#29156B",
-        },
-        error: {
-          text: "#903534",
-          icon: "#d1080c",
-          background: "#f1e7e6",
-        },
-        "red-err": "#AF3C43",
+        "gray-light-200": "#f5f5f5",
+        "gray-light-100": "#e0e0e0",
+        "gray-light": "#EFEFEF",
+        "gray-md": "#CFCFCF",
+        "gray-dark": "#444",
+        "gray-dark-100": "#333",
+        "text-gray": "#5c5c5c",
+        "dk-blue": "#26374a",
+        "hr-red-bar": "#AE3B43",
+        "canada-ca-link-colour": "#7834BC",
+        "canada-footer-font": "#284162",
+        "canada-footer-hover-font-blue": "#0535d2",
+        "footer-background-color": "#f8f8f8",
       },
     },
   },
   variants: {
     extend: {
-      backgroundColor: ["active", "hover"],
+      backgroundColor: ["active"],
       textColor: ["visited"],
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    // ...
+  ],
 };
