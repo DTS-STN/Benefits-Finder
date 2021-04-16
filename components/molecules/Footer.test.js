2e413c6a5b9fbb12e6f6cf54892f31dcb134dcd7
var-kyle <kyle.meelker@gmail.com>
2021-04-16 09:28:31 -0400

    update footer

--- components/molecules/Footer.test.js
+++ components/molecules/Footer.test.js
@@ -1,38 +1,19 @@
+import React from "react";
 import { render, screen } from "@testing-library/react";
-import { axe, toHaveNoViolations } from "jest-axe";
 import { Primary } from "./Footer.stories";
 
-expect.extend(toHaveNoViolations);
+it("renders Footer in its primary state", () => {
+  render(<Primary {...Primary.args} />);
 
-describe("Footer", () => {
-  it("renders with government links", async () => {
-    render(<Primary {...Primary.args} />);
-    expect(await screen.findByText("Contact us")).toBeTruthy();
-    expect(await screen.findByText("Departments and agencies")).toBeTruthy();
-    expect(await screen.findByText("Public service and military")).toBeTruthy();
-    expect(await screen.findByText("News")).toBeTruthy();
-    expect(
-      await screen.findByText("Treaties, laws and regulations")
-    ).toBeTruthy();
-    expect(await screen.findByText("Government-wide reporting")).toBeTruthy();
-    expect(await screen.findByText("Prime Minister")).toBeTruthy();
-    expect(await screen.findByText("How government works")).toBeTruthy();
-    expect(await screen.findByText("Open government")).toBeTruthy();
+  expect(
+    screen.getByAltText("Symbol of the Government of Canada")
+  ).toBeTruthy();
+
+  Primary.args.footerBoxLinks.forEach((value) => {
+    screen.getByText(value.footerBoxLinkText);
   });
 
-  it("renders with about links", async () => {
-    render(<Primary {...Primary.args} />);
-    expect(await screen.findByText("Social media")).toBeTruthy();
-    expect(await screen.findByText("Mobile applications")).toBeTruthy();
-    expect(await screen.findByText("About Canada.ca")).toBeTruthy();
-    expect(await screen.findByText("Terms and conditions")).toBeTruthy();
-    expect(await screen.findByText("Privacy")).toBeTruthy();
-  });
-
-  it("has no a11y violations", async () => {
-    const { container } = render(<Primary {...Primary.args} />);
-    const results = await axe(container);
-
-    expect(results).toHaveNoViolations();
+  Primary.args.links.forEach((value) => {
+    screen.getByText(value.linkText);
   });
 });
