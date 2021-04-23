cf8f27f2cb3808b543e6e6a8b695f3a392b7daaa
var-kyle <kyle.meelker@gmail.com>
2021-04-23 14:04:47 -0400

    update source of popular categories

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
-const { i18n } = require('./next-i18next.config')
+const { i18n } = require("./next-i18next.config");
+const { getDomain } = require("./lib/utils");
 
 module.exports = {
   i18n,
-}
+  images: {
+    domains: [`${getDomain(process.env.NEXT_PUBLIC_STRAPI_API_URL)}`],
+  },
+};
