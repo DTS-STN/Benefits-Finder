dce539434fdb13734e00b9d331a1a6143c11f60b
var-kyle <kyle.meelker@gmail.com>
2021-04-26 09:25:04 -0400

    update next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ const { getDomain } = require("./lib/utils");
 module.exports = {
   i18n,
   images: {
-    domains: [`${getDomain(process.env.NEXT_PUBLIC_STRAPI_API_URL)}`],
+    domains: ["localhost"],
   },
 };
