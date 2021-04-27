64a32ba7891e2b0684cbddf2f5ec92bf6f058571
var-kyle <kyle.meelker@gmail.com>
2021-04-27 12:02:47 -0400

    update test and storybook for card grid to make more sense

--- components/organisms/CardGrid.stories.js
+++ components/organisms/CardGrid.stories.js
@@ -8,69 +8,65 @@ export default {
   component: CardGrid,
 };
 
-export const benefitCards = () => {
-  return [
-    <BenefitCard
-      key="1"
-      id="1"
-      title="benefit title 1"
-      description="benefit description 1"
-      applyLink="#"
-      type="benefit type 1"
-      program="program 1"
-    />,
-    <BenefitCard
-      key="2"
-      id="2"
-      title="benefit title 2"
-      description="benefit description 2"
-      applyLink="#"
-      type="benefit type 2"
-      program="program 2"
-    />,
-    <BenefitCard
-      key="3"
-      id="3"
-      title="benefit title 3"
-      description="benefit description 3"
-      applyLink="#"
-      type="benefit type 3"
-      program="program 3"
-    />,
-  ];
-};
+const benefitCards = [
+  <BenefitCard
+    key="1"
+    id="1"
+    title="benefit title 1"
+    description="benefit description 1"
+    applyLink="#"
+    type="benefit type 1"
+    program="program 1"
+  />,
+  <BenefitCard
+    key="2"
+    id="2"
+    title="benefit title 2"
+    description="benefit description 2"
+    applyLink="#"
+    type="benefit type 2"
+    program="program 2"
+  />,
+  <BenefitCard
+    key="3"
+    id="3"
+    title="benefit title 3"
+    description="benefit description 3"
+    applyLink="#"
+    type="benefit type 3"
+    program="program 3"
+  />,
+];
 
-export const categoryCards = () => {
-  return [
-    <PopularCategoryCard
-      key="1"
-      id="1"
-      title="category title 1"
-      description="category description 1"
-      imgSource="/landscape.png"
-      imgAltText="image alt text 1"
-      type="category type 1"
-    />,
-    <PopularCategoryCard
-      key="2"
-      id="2"
-      title="category title 2"
-      description="category description 2"
-      imgSource="/landscape.png"
-      imgAltText="image alt text 2"
-      type="category type 2"
-    />,
-    <PopularCategoryCard
-      key="3"
-      id="3"
-      title="category title 3"
-      description="category description 3"
-      imgSource="/landscape.png"
-      imgAltText="image alt text 3"
-      type="category type 3"
-    />,
-  ];
-};
+const categoryCards = [
+  <PopularCategoryCard
+    key="1"
+    id="1"
+    title="category title 1"
+    description="category description 1"
+    imgSource="/landscape.png"
+    imgAltText="image alt text 1"
+    type="category type 1"
+  />,
+  <PopularCategoryCard
+    key="2"
+    id="2"
+    title="category title 2"
+    description="category description 2"
+    imgSource="/landscape.png"
+    imgAltText="image alt text 2"
+    type="category type 2"
+  />,
+  <PopularCategoryCard
+    key="3"
+    id="3"
+    title="category title 3"
+    description="category description 3"
+    imgSource="/landscape.png"
+    imgAltText="image alt text 3"
+    type="category type 3"
+  />,
+];
 
 const Template = (args) => <CardGrid {...args}></CardGrid>;
 
@@ -78,9 +74,9 @@ export const BenefitGrid = Template.bind({});
 export const CategoryGrid = Template.bind({});
 
 BenefitGrid.args = {
-  children: benefitCards(),
+  children: benefitCards,
 };
 
 CategoryGrid.args = {
-  children: categoryCards(),
+  children: categoryCards,
 };
--- components/organisms/CardGrid.test.js
+++ components/organisms/CardGrid.test.js
@@ -1,11 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { axe, toHaveNoViolations } from "jest-axe";
-import {
-  BenefitGrid,
-  CategoryGrid,
-  benefitCards,
-  categoryCards,
-} from "./CardGrid.stories";
+import { BenefitGrid, CategoryGrid } from "./CardGrid.stories";
 
 expect.extend(toHaveNoViolations);
 
@@ -13,9 +8,8 @@ describe("CardGrid tests", () => {
   it("renders a Benefit Grid in its primary state", () => {
     render(<BenefitGrid {...BenefitGrid.args} />);
 
-    for (let i in benefitCards.length) {
-      let benefitData = benefitCards[i];
-      let benefitCard = screen.getByText(benefitData.title);
+    for (let i in 3) {
+      let benefitCard = screen.getByText(`benefit title ${i}`);
       expect(benefitCard).toBeTruthy();
     }
   });
@@ -23,9 +17,8 @@ describe("CardGrid tests", () => {
   it("renders a Category Grid in its primary state", () => {
     render(<CategoryGrid {...CategoryGrid.args} />);
 
-    for (let i in categoryCards.length) {
-      let data = categoryCards[i];
-      let categoryCard = screen.getByText(data.title);
+    for (let i in 3) {
+      let categoryCard = screen.getByText(`category title ${i}`);
       expect(categoryCard).toBeTruthy();
     }
   });
