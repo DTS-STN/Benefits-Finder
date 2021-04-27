2bf301392047e2909e2dca0fa6d22a78197dcd38
shewood <shewood01@gmail.com>
2021-04-27 12:13:35 -0400

    small change

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -2,46 +2,38 @@
 // /// <reference types="Cypress" />
 
 // Define at the top of the spec file or just import it
-  function checkA11y(cy) {
-    cy.checkA11y({
-      runonly: {
-        type: 'tag',
-        values: ['wcag2a', 'wcag2aa'],
-      },
-    })
-  }
-  
-
-
+function checkA11y(cy) {
+  cy.checkA11y({
+    runonly: {
+      type: "tag",
+      values: ["wcag2a", "wcag2aa"],
+    },
+  });
+}
 
 describe("home page", () => {
-    beforeEach(() => {
-        cy.visit("/");
-        cy.injectAxe()
-    
-    });
+  beforeEach(() => {
+    cy.visit("/");
+    cy.injectAxe();
+  });
 
-    it("landing page loaded", () =>{
-        cy.url().should("contains", "/");
-       
-        
-    });
+  it("landing page loaded", () => {
+    cy.url().should("contains", "/");
+  });
 
-    it('Checkes a11y violations', () => {
-        cy.checkA11y()
-      })
+  it("Checkes a11y violations", () => {
+    cy.checkA11y();
+  });
 
-   // cy.task('log', 'This will be output to the terminal')
-    it("Toggles content language when language button is clicked", () => {
-        cy.get('[data-cy="toggle-language-link"]').click()
-        cy.url().should('eq', 'http://localhost:3000/fr')
-    });
+  it("Toggles content language when language button is clicked", () => {
+    cy.get('[data-cy="toggle-language-link"]').click();
+    cy.url().should("eq", "http://localhost:3000/fr");
+  });
 
-    it ("Alpha or Early Access Banner is present ", ()=> {
-        cy.get('[data-cy=alpha-banner-text]').should("be.visible");
-        cy.get('[data-cy=alpha-banner-description]').should("be.visible");
-        cy.get('[data-cy=back-to-active-experiments]').should("be.visible");
-        cy.get('[data-cy=back-to-active-experiments]').should("be.visible");
-    })
-   
-})
+  it("Alpha or Early Access Banner is present ", () => {
+    cy.get("[data-cy=alpha-banner-text]").should("be.visible");
+    cy.get("[data-cy=alpha-banner-description]").should("be.visible");
+    cy.get("[data-cy=back-to-active-experiments]").should("be.visible");
+    cy.get("[data-cy=back-to-active-experiments]").should("be.visible");
+  });
+});
