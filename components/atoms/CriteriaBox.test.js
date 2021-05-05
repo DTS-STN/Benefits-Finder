db93d63750c4d691913468f716133ec6269bab67
shewood <shewood01@gmail.com>
2021-05-05 09:28:40 -0400

    added a11y test for criteriaBox

--- components/atoms/CriteriaBox.test.js
+++ components/atoms/CriteriaBox.test.js
@@ -3,8 +3,17 @@ import "@testing-library/jest-dom/extend-expect";
 import { axe, toHaveNoViolations } from "jest-axe";
 import { Primary } from "./CriteriaBox.stories";
 
+expect.extend(toHaveNoViolations);
+
 it("renders CriteriaBox in its primary state", () => {
   render(<Primary {...Primary.args} />);
 
   expect(screen.getByText("Title Text")).toBeTruthy();
+});
+
+it("has no a11y violations", async () => {
+  const { container } = render(<Primary {...Primary.args} />);
+  const results = await axe(container);
+
+  expect(results).toHaveNoViolations();
 });
