7b6fcd1ac7fe1cd31a5a5311784a0ae1d0632db5
Tisham Islam <45071113+sl1mt1sh@users.noreply.github.com>
2021-05-21 12:03:07 -0400

    cleaned up code, ran npm i

--- pages/api/assumptions.js
+++ pages/api/assumptions.js
@@ -1,19 +1,16 @@
 export default async (req, res) => {
   let rawLocation;
   const ip = req.headers["ip-address"];
-  console.log("before if");
+  //check if the request requirements are valid to get the location
   if (req.method === "GET" && req.headers["assumption-type"] == "location") {
     try {
-      console.log("in try bloc, ip = " + ip);
       const apiResponse = await fetch(
-        process.env.IP_LOCATION_API_URL + "?ip=" + ip
+        process.env.IP_LOCATION_API_URL + "?ip=" + ip //use the secret api call to get the user location
       );
       rawLocation = await apiResponse.json();
-      console.log(rawLocation);
     } catch (e) {
-      rawLocation = ""; //default value
+      rawLocation = {}; //default value so no errors happen
     }
     res.status(200).json(rawLocation);
   }
-  console.log("after if");
 };
