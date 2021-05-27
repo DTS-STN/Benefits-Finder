c3f2b9b00ebe2731fba60326423e0da3794eef3e
Thomas Dacey <DaceyTom2@gmail.com>
2021-05-27 15:09:50 -0400

    updated situation api to make location assumption

--- pages/api/situation.js
+++ pages/api/situation.js
@@ -1,9 +1,31 @@
 import cookie from "cookie";
 
-export default (req, res) => {
+export default async (req, res) => {
   // update the user situation cookie
   if (req.method === "POST") {
-    const situation = req.body.situation || {};
+    console.log(req.body.situation);
+    let situation = req.body.situation || {};
+
+    if (
+      req.cookies.situation === undefined &&
+      req.connection.remoteAddress != "127.0.0.1"
+    ) {
+      //check if the request requirements are valid to get the location
+      let rawLocation;
+      try {
+        const apiResponse = await fetch(
+          process.env.IP_LOCATION_API_URL +
+            "?ip=" +
+            req.connection.remoteAddress //use the secret api call to get the user location
+        );
+        rawLocation = await apiResponse.json();
+      } catch (e) {
+        rawLocation = {}; //default value so no errors happen
+      }
+      let location =
+        rawLocation?.country_code3 === "CAN" ? rawLocation.state_prov : ""; //if in canada, return the prov, otherwise return an empty string,
+      situation = { location: location };
+    }
 
     res.setHeader(
       "Set-Cookie",
