fb1d3bc6f0255ea97074dd97f3b3fd2e08e5cb01
shewood <28784886+shewood@users.noreply.github.com>
2021-06-03 14:11:16 -0400

    added NEXT_ to the args in docker and files (#91)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const { i18n } = require("./next-i18next.config");
 module.exports = {
   i18n,
   images: {
-    domains: ["localhost", "benefits-finder-cms-api-dev.dev.dts-stn.com"],
+    domains: ["localhost", "benefits-finder-cms-api-dev.dev.dts-stn.com" ],
   },
+  env: {
+    NEXT_IP_LOCATION_API_URL: process.env.NEXT_IP_LOCATION_API_URL,
+  }
 };
